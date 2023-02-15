51ae4a4d014267105c7bd5a041673c864315a567
Nicolas Dermine <nicolas.dermine@gmail.com>
2023-02-15 07:32:29 +0100

    Exercise 1: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('');
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(guess);
+        setGuess('');
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+        minLength="5"
+        maxLength="5"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
